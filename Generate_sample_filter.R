b1017e73d84260dfae74a03d835142bd607e08ea
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-05-21 11:50:36 -0700

    Update Generate_sample_filter.R

--- Generate_sample_filter.R
+++ Generate_sample_filter.R
@@ -2,22 +2,22 @@
 
 library(SeqVarTools)
 
-gds <- seqOpen("/home/hkings/DATA/CFF_5134_onlyGT.gds")
+gds <- seqOpen("/home/hkings/DATA/CFF_pid.gds")
 gds.id <- seqGetData(gds, "sample.id")
 
 phen <- read.table("phenotype.txt", header = TRUE)
 
-sum(phen$vcf_id %in% gds.id)
+sum(phen$pid %in% gds.id)
 #[1] 5134
-sum(gds.id %in% phen$vcf_id)
+sum(gds.id %in% phen$pid)
 #[1] 5134
 #Both columns must have all the same smaple IDs
 #Check that they are also in the same order... THIS MUST BE TRUE
-identical(as.character(phen$vcf_id), as.character(gds.id))
+identical(as.character(phen$pid), as.character(gds.id))
 
 #Mine were not in the same order!, so must do this (can only do if the %in% checks are both equal):
-phen <- phen[match(gds.id, phen$vcf_id),]
-identical(as.character(phen$vcf_id), as.character(gds.id))
+phen <- phen[match(gds.id, phen$pid),]
+identical(as.character(phen$pid), as.character(gds.id))
 
 
 #Get a boolean vector of samples to exclude based on QC in phenotype data
