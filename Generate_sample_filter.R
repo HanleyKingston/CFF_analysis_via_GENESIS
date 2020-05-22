b258ac3fb4ce67c12823e91a5ff32b672830d75a
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-05-21 19:20:53 -0700

    Update Generate_sample_filter.R

--- Generate_sample_filter.R
+++ Generate_sample_filter.R
@@ -13,11 +13,12 @@ sum(phen$sid %in% gds.id)
 sum(gds.id %in% phen$sid)
 #[1] 5134
 
-#Mine were not in the same order!, so must do this (can only do if the %in% checks are both equal):
-phenotype <- phenotype[match(gds.id, phenotype$sid),]
+
 #THIS MUST BE TRUE
 identical(as.character(phenotype$sid), as.character(gds.id))
 
+#These should be in the same order from the last script, but if not, must do this (can only do if the %in% checks are both equal):
+phenotype <- phenotype[match(gds.id, phenotype$sid),]
 
 #Get a boolean vector of samples to exclude based on QC in phenotype data
 #Must make sure to exclude all individuals who whould be excluded based on phenotype QC
