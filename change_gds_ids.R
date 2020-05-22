d23f533997c44b55630f1c9ac302d390d3667340
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-05-21 19:05:42 -0700

    Update change_gds_ids.R

--- change_gds_ids.R
+++ change_gds_ids.R
@@ -1,31 +1,27 @@
 library(SeqArray)
 
-gds <- openfn.gds("CFF_pid.gds", readonly=FALSE)
 #Note: I made a copy of my GDS file before changing the sample_ids, which is why the name doesn't match the original gds file
+gds <- openfn.gds("CFF_sid_onlyGT.gds", readonly=FALSE)
 
 phenotype <- read.table("phenotype.txt", header = TRUE)
 
 gds.id <- scan("sample_id_gds.txt", "character", sep = "\n")
 #Read 5134 items
 
-identical(as.character(phenotype$vcf_id), as.character(gds.id))
-#[1] FALSE #No idea why this is false, but everything else I checked suggests they are identical
 
-df <-data.frame(gds.id = NA, phenotype.id = NA)
-for(i in 1:length(gds.id)){
-  if(as.character(gds.id[i]) != as.character(phenotype$vcf_id[i])){
-  df[i,] <- c(gds.id, phenotype$vcf.id)
-    }}
-   
-
+sum(as.character(phenotype$vcf_id) %in% as.character(gds.id))
+#[1] 5134
+phenotype <- phenotype[match(gds.id, phenotype$vcf_id),]
+identical(as.character(phenotype$vcf_id), as.character(gds.id)) #This must be TRUE!
+#[1] TRUE
 
 add.gdsn(gds, "sample.id", phenotype$pid, replace=TRUE, compress="LZMA_RA", closezip=TRUE)
 closefn.gds(gds)
 
-gds <- seqOpen("CFF_pid.gds")
+gds <- seqOpen("CFF_sid_onlyGT.gds")
 head(seqGetData(gds, "sample.id"))
 
 #check:
-identical(as.character(seqGetData(gds, "sample.id")), as.character(phenotype$pid))
+identical(as.character(seqGetData(gds, "sample.id")), as.character(phenotype$pid)) #must be TRUE!
 
 seqClose(gds)
