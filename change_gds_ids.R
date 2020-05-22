82c81e1984d51f477ea5779b0d45a68c1a513ff8
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-05-21 19:10:35 -0700

    Update change_gds_ids.R

--- change_gds_ids.R
+++ change_gds_ids.R
@@ -15,13 +15,13 @@ phenotype <- phenotype[match(gds.id, phenotype$vcf_id),]
 identical(as.character(phenotype$vcf_id), as.character(gds.id)) #This must be TRUE!
 #[1] TRUE
 
-add.gdsn(gds, "sample.id", phenotype$pid, replace=TRUE, compress="LZMA_RA", closezip=TRUE)
+add.gdsn(gds, "sample.id", phenotype$sid, replace=TRUE, compress="LZMA_RA", closezip=TRUE)
 closefn.gds(gds)
 
 gds <- seqOpen("CFF_sid_onlyGT.gds")
 head(seqGetData(gds, "sample.id"))
 
 #check:
-identical(as.character(seqGetData(gds, "sample.id")), as.character(phenotype$pid)) #must be TRUE!
+identical(as.character(seqGetData(gds, "sample.id")), as.character(phenotype$sid)) #must be TRUE!
 
 seqClose(gds)
