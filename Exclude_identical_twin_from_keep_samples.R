433d9a630e11861a11eee49da724644b5f6e9c39
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-05-26 12:17:44 -0700

    Update and rename Generate_annotated_phenotype_df.R to Exclude_identical_twin_from_keep_samples.R

--- Exclude_identical_twin_from_keep_samples.R
+++ Exclude_identical_twin_from_keep_samples.R
@@ -1,17 +1,18 @@
 #Identify identical twins & remove from analysis
 
 #Read in PCrelate object and existing keep_samples vector
-pcrel2 <- readRDS(file = "pcr_grm_LDsqrt0.1.rds")
+pcrel2 <- readRDS(file = "CFF_LDsqrt0.1pcr_obj.rds")
 kinship <- pcrel2$kinBtwn
 keep_samples <- readRDS(file = "keep_samples.rds")
 
 #Find likely identical twins
 range(kinship$kin)
-kinship[kinship$kin > 0.5, c("kin", "k2", "ID1", "ID2")]
+kinship[kinship$kin > 0.40, c("kin", "k2", "ID1", "ID2")]
 
 #Remove identical twins from keep_samples vector
 exclude_twins <- kinship[kinship$kin > 0.5, "ID1"] #ID1 is always the lowest of the 2 number IDs
 "%notin%" <- Negate("%in%")
-keep_samples <- keep_samples[keep_samples %notin% exclude_twins]
+keep_samples_noTwins <- keep_samples[keep_samples %notin% exclude_twins]
 
-saveRDS(keep_samples, file = "keep_samples.rds")
+saveRDS(keep_samples_noTwins, file = "keep_samples_noTwins.rds")
+
