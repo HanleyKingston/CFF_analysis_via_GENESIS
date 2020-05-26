15b8aa1ee377d97691af9357835b99aff75e5369
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-05-26 12:18:35 -0700

    Update PC_and_GRM_plots.R

--- PC_and_GRM_plots.R
+++ PC_and_GRM_plots.R
@@ -15,13 +15,50 @@ phenotype <- phenotype[phenotype$sid %in% keep_samples,]
 
 
 pca <- readRDS("CFF_LDsqrt0.1pcair.rds")
-pcs.df <- as.data.frame(pca$vectors[,1:4])
+pcs.df <- as.data.frame(pca$vectors[,1:7])
 pcs.df$sid <- rownames(pcs.df)
-pcs.df <- merge(pcs.df, phenotype[,c("sid","site")], by="sid")
+pcs.df <- merge(pcs.df, phenotype[,c("sid","site", "race_or_ethnicity")], by="sid")
 
 rels_V <- pca$rels
 pcs.df$relate <- ifelse(pcs.df$sid %in% rels_V, "related", "unrelated")
 
+pdf("/home/hkings/DATA/PC1and2.pdf")
+ggplot() +
+   geom_point(aes(pcs.df[,2], pcs.df[,3], col = factor(pcs.df$race_or_ethnicity), pch = pcs.df$relate)) +
+   xlab("PC 1") + ylab("PC 2") +
+   scale_shape(solid = FALSE) +
+   labs(col= "Race or Ethnicity", shape="PC set")
+dev.off()
+
+#Zoom in:
+pdf("/home/hkings/DATA/PC1and2_zoomed.pdf")
+ggplot() +
+   geom_point(aes(pcs.df[,2], pcs.df[,3], col = factor(pcs.df$race_or_ethnicity), pch = pcs.df$relate)) +
+   xlab("PC 1") + ylab("PC 2") +
+   scale_shape(solid = FALSE) +
+   labs(col= "Race or Ethnicity", shape="PC set") +
+   ylim(c(-0.1,0.1)) +
+   xlim(c(-0.1,0.1))
+dev.off()
+
+
+pdf("/home/hkings/DATA/PC2and3.pdf")
+ggplot() +
+   geom_point(aes(pcs.df[,3], pcs.df[,4], col = factor(pcs.df$race_or_ethnicity), pch = pcs.df$relate)) +
+   xlab("PC 2") + ylab("PC 3") +
+   scale_shape(solid = FALSE) +
+   labs(col= "Race or Ethnicity", shape="PC set")
+dev.off()
+
+pdf("/home/hkings/DATA/PC3and4.pdf")
+ggplot() +
+   geom_point(aes(pcs.df[,4], pcs.df[,5], col = factor(pcs.df$race_or_ethnicity), pch = pcs.df$relate)) +
+   xlab("PC 3") + ylab("PC 4") +
+   scale_shape(solid = FALSE) +
+   labs(col= "Race or Ethnicity", shape="PC set")
+dev.off()
+
+#Check impact of site and PC relatedness assignment on results
 pdf("/home/hkings/DATA/PC1and2.pdf")
 ggplot() +
    geom_point(aes(pcs.df[,2], pcs.df[,3], col = factor(pcs.df$site), pch = pcs.df$relate)) +
@@ -30,32 +67,16 @@ ggplot() +
    labs(col= "Study", shape="PC set")
 dev.off()
 
-pdf("/home/hkings/DATA/PC2and3.pdf")
-ggplot() +
-   geom_point(aes(pcs.df[,3], pcs.df[,4], col = factor(pcs.df$site), pch = pcs.df$relate)) +
-   xlab("PC 2") + ylab("PC 3") +
-   scale_shape(solid = FALSE) +
-   labs(col= "Study", shape="PC set")
-dev.off()
-
-
-pdf("/home/hkings/DATA/PC3and4.pdf")
-ggplot() +
-   geom_point(aes(pcs.df[,4], pcs.df[,5], col = factor(pcs.df$site), pch = pcs.df$relate)) +
-   xlab("PC 3") + ylab("PC 4") +
-   scale_shape(solid = FALSE) +
-   labs(col= "Study", shape="PC set")
-dev.off()
-
 #plot second iteration kinship estimates:
-pcrel <- readRDS(file = "CFF_LDsqrt0.1pcr_grm.rds")
+pcrel <- readRDS(file = "CFF_LDsqrt0.1pcr_obj.rds")
 kinship <- pcrel$kinBtwn
 
 #plot 2nd iteration kinship:
 kinship$site  <- sub("_.*", "", kinship$ID1) #This may not be perfectly accurate because some individuals were included in multiple studies and some vcf_ids have changed
+kinship$race_or_ethnicity  <- phenotype$race_or_ethnicity
 
 png("kinship.png")
-ggplot(kinship, aes(k0, kin, col = factor(kinship$site))) +
+ggplot(kinship, aes(k0, kin, col = kinship$race_or_ethnicity)) + #shape = factor(kinship$site),
     geom_hline(yintercept=2^(-seq(3,9,2)/2), linetype="dashed", color = "grey") +
     geom_point(alpha=0.2) +
     ylab("kinship estimate") +
@@ -63,8 +84,6 @@ ggplot(kinship, aes(k0, kin, col = factor(kinship$site))) +
 dev.off()
 
 #Plot percent variance explained by each pc:
-pca <- readRDS("pcair_LDsqrt0.1.rds")
-
 pcs.df2 <- as.data.frame(pca$vectors[pca$unrels,])
 names(pcs.df2) <- paste0("PC", 1:ncol(pcs.df2))
 pcs.df2$sample.id <- rownames(pcs.df2)
