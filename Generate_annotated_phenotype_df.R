276e67b1ac1d40ba66c02999013cab494be9c0e5
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-05-26 13:48:17 -0700

    Update Generate_annotated_phenotype_df.R

--- Generate_annotated_phenotype_df.R
+++ Generate_annotated_phenotype_df.R
@@ -10,5 +10,8 @@ library(SeqVarTools)
 gdsfmt::showfile.gds(closeall=TRUE)
 gds <- seqOpen("CFF_sid_onlyGT.gds")
 gds.id <- seqGetData(gds, "sample.id")
-sum(gds.id %in% phenotype$sample.id) == sum(phenotype$sample.id %in% gds.id)
-identical(as.character(phenotype$sample.id), gds.id)
+sum(gds.id %in% phenotype$sample.id) == sum(phenotype$sample.id %in% gds.id) #phenotype file isn't fitlered so will have more total samples
+keep_samples <- readRDS("keep_samples.rds")
+
+
+identical(as.character(phenotype[phenotype$sample.id %in% keep_samples, "sample.id"]), gds.id)
