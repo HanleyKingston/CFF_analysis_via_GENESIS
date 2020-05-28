c0c1815b7e3e54d3ae263ea1c8893fc1a4758cce
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-05-27 19:12:52 -0700

    Update change_gds_ids.R

--- change_gds_ids.R
+++ change_gds_ids.R
@@ -3,38 +3,40 @@ library(SeqArray)
 gds.id <- seqGetData(seqOpen("CFF_5134_onlyGT.gds"), "sample.id")
 sample_key <- read.table("sample_names_key.txt", header = TRUE)
 
-sum(duplicated(sample_key$vcf_id))
+sum(duplicated(sample_key$vcf_id)) #Must be 0, otherwise, can't compair to gds file
 #[1] 0
 
-sum(as.character(sample_key$vcf_id) %in% as.character(gds.id))
+sample_key <- sample_key[match(gds.id, sample_key$vcf_id),]
+nrow(sample_key)
 #[1] 5134
 
-sample_key[match(sample_key$vcf_id,]
-identical(as.character(sample_key[sample_key$vcf_id %in% gds.id, "vcf_id"]), as.character(gds.id))
+identical(as.character(sample_key$vcf_id), as.character(gds.id)) #This must be TRUE!
 
-sid_list <- as.vector(sample_key[sample_key$vcf_id %in% gds.id, "sid"])
+sid_list <- as.vector(as.character(sample_key[sample_key$vcf_id %in% gds.id, "sid"]))
 
 class(sid_list)
 #[1] "character"
 
-identical(as.character(phenotype$vcf_id), as.character(gds.id)) #This must be TRUE!
-#[1] FALSE
+#Close gds file and reopen in write mode
+gdsfmt::showfile.gds(closeall=TRUE)
+rm(gds.id)
+gds <- openfn.gds("CFF_sid_onlyGT.gds", readonly=FALSE)
 
-#Can coerce to same order (if all the same IDs are shared) with this:
-phenotype <- phenotype[match(gds.id, phenotype$vcf_id),]
-
-sid <- as.character(phenotype$sid)
-
-#Save re-ordered phenotype file:
-write.table(phenotype, "phenotype.txt", sep = "\t")
-
-add.gdsn(gds, "sample.id", sid, replace=TRUE, compress="LZMA_RA", closezip=TRUE)
+add.gdsn(gds, "sample.id", sid_list, replace=TRUE, compress="LZMA_RA", closezip=TRUE)
 closefn.gds(gds)
 
 gds <- seqOpen("CFF_sid_onlyGT.gds")
-head(seqGetData(gds, "sample.id"))
 
+#Check:
+gds.id <- seqGetData(gds, "sample.id")
+class(gds.id)
+#[1] "character"
+is.vector(gds.id)
+#[1] TRUE
 #check:
-identical(seqGetData(gds, "sample.id"), as.character(phenotype$sid)) #must be TRUE!
+identical(gds.id, as.character(sample_key$sid)) #must be TRUE!
+
+#Save a vector of gds IDs:
+saveRDS(gds.id, "gds_id.rds")
 
 seqClose(gds)
