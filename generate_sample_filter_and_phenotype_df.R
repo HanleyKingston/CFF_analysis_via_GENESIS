cd056a56e847549f28c78ce2ce55c5e8b795fac7
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-05-27 17:40:27 -0700

    Update and rename flag_exclude_in_participants.R to generate_sample_filter_and_phenotype_df.R

--- generate_sample_filter_and_phenotype_df.R
+++ generate_sample_filter_and_phenotype_df.R
@@ -1,80 +1,26 @@
 participants <- read.delim("participants_cffwgs.tsv", sep = "\t", header = TRUE)
-key <- read.delim("key_cffwgs.tsv", sep = "\t", header = TRUE)
-nrow(key)
+
+
+#Filter based on sid_pid_to_keep list
+sample_key <- read.table("sample_names_key.txt", header = TRUE)
+
+nrow(sample_key)
 #[1] 5199
-flag <- scan("samples_flag.txt", "character", sep = "\n")
-#Read 79 items
-exclude <- scan("samples_exclude.txt", "character", sep = "\n")
-#Read 51 items
-gds.id <- scan("sample_id_gds.txt", "character", sep = "\n")
-#Read 5134 items
 
-"%notin%" <- Negate("%in%")
+sum(participants$pid %in% sample_key$pid)
+#[1] 5161
 
-#Key has pid, sid, and vcf_id, participants only has pid... I will use the sid in analysis
-sum(participants$pid %in% key$pid)
-#[1] 5161 #All participants are in the key
-sum(key$pid %in% participants$pid)
-#[1] 5199 #All indiviudals in key are in participants, so 38 people must be repeated in the key
-sum(duplicated(key$pid))
-#[1] 38 #38 pairs of duplicates, so difference between key and participants file is just that duplicates were removed
-#I only want to keep one of each duplicate... keep the person who is in the gds file
-#Get all duplicates:
-key[duplicated(key$pid) | duplicated(key$pid, fromLast = TRUE),]
+participants <- merge(participants, sample_key, by = "pid")
+#[1] 5161
 
-#Because each duplicate has a different vcf_id and sid, I want to keep the one in the gds.id file
-duplicates <- key[duplicated(key$pid) | duplicated(key$pid, fromLast = TRUE), "vcf_id"]
-length(duplicates)
-#[1] 72
-sum(duplicates %in% gds.id)
-#[1] 71
-#all but one of the duplicates are in the gds file... since I can't determine which is the correct sample, I will remove them from the analysis entirely?
-sum(in_key_not_gds %in% duplicates)
-#[1] 1 #One of the IDs that are in the key but not the gds are in duplicates
-sum(exclude %in% duplicates)
-#[1] 0
+completely remove anyone from participants not in gds file
+library(SeqArray)
+gds.id <- seqGetData(seqOpen("CFF_5134_onlyGT.gds"), "sample.id")
+nrow(participants[participants$VCF_ID %in% gds.id,])
 
+#Save sample filter
+keep_samples <- as.vector(sample_key$sid)
 
-#key[duplicated(key$pid) | duplicated(key$pid, fromLast = TRUE), c("sid", "vcf_id")] <- "NA"
-nrow(key)
-#[1] 5199
-sum(key$vcf_id %notin% gds.id)
-#[1] 65
-
-#Add key and phenotype together so phenotype data contains the vcf_id
-phenotype <- merge(participants, key, by = "pid", all = TRUE)
-
-phenotype <- phenotype[order(phenotype$vcf_id),]
-gds.id <- sort(gds.id)
-
-sum(phenotype$vcf_id %notin% gds.id)
-#[1] 65
-not_shared_in_phenotype <- phenotype[phenotype$vcf_id %notin% gds.id, "vcf_id"]
-sum(exclude %in% not_shared_in_phenotype)
-#[1] 0
-sum(flag %in% not_shared_in_phenotype)
-#[1] 18
-
-
-sum(gds.id %notin% phenotype$vcf_id)
-#[1] 0 #There are no samples in the gds file without corresponding phenotype info
-
-#anything in phenotype file without a corresponding entry in gds file should be removed, otherwise analysis won't run
-phenotype_pruned <- phenotype[phenotype$vcf_id %in% gds.id,]
-nrow(phenotype_pruned)
-#[1] 5134
-identical(as.character(phenotype_pruned$vcf_id), gds.id)
-#[1] TRUE
-
-#find participants without necissary phenotype info
-nrow(phenotype_pruned[is.na(phenotype_pruned$cftr_var_1_wgs) | is.na(phenotype_pruned$cftr_var_2_wgs),]) #Note: 609 people are missing a knorma value
-#[1] 57
-missing_F508 <- phenotype_pruned[is.na(phenotype_pruned$cftr_var_1_wgs) | is.na(phenotype_pruned$cftr_var_2_wgs), "vcf_id"] #Note: 609 people are missing a knorma value
-sum(missing_F508 %in% exclude)
-#[1] 14
-
-phenotype_pruned$include_in_analysis <- ifelse(phenotype_pruned$vcf_id %in% duplicates | phenotype_pruned$vcf_id %in% exclude | phenotype_pruned$vcf_id %in% missing_F508,
-                                               NA, ifelse(phenotype_pruned$vcf_id %in% flag, "flag", "keep"))
 
 #Create a column for site (This may not be perfectly accurate because some individuals were included in multiple studies and some vcf_ids have changed)
 phenotype_pruned$site  <- sub("_.*", "", phenotype_pruned$vcf_id)
