689d73ebbc001e568b68a21c19bcfe8f1ab7d4b4
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-02 11:30:20 -0700

    Update generate_sample_filter_and_phenotype_df.R

--- generate_sample_filter_and_phenotype_df.R
+++ generate_sample_filter_and_phenotype_df.R
@@ -24,10 +24,11 @@ length(gds.id)
 #[1] 5134
 sum(participants2$sid %in% gds.id)
 
-#Filtering sample key fist on sid ensures that the right partiicpant samples of the duplicated pids are included  in the participant phenotype df
-phenotype_pruned <- participants2[participants2$sid %in% gds.id,]
+#Filtering sample key fist on sid ensures that the right partiicpant samples of the duplicated pids are included  in the participant phenotype df & order to match gds file:
+phenotype_pruned <- participants2[match(gds.id, participants2$sid),]
 nrow(phenotype_pruned)
 #[1] 5134
+identical(as.character(phenotype_pruned$sid), gds.id)
 
 #Create include in analysis filter:
 #Read in keep_samples by sid fitler (base on QC and duplicates...)
@@ -100,40 +101,8 @@ dev.off()
 
 #To make more managable, I'm just selecting phenotypes I'm interested in
 phenotype_pruned_selectCol  <- phenotype_pruned[,c("pid", "sid", "sex_wgs", "birthdate_year", "cftr_var_1_wgs", "cftr_var_2_wgs", "age_death", "knorma", "vcf_id", "include_in_analysis", "site", "F508_count", "race_or_ethnicity")]
-
+    
 saveRDS(phenotype_pruned_selectCol, "phenotype.rds")
-  
-    
-#Extra...ignore:    
-#Filter based on sid_pid_to_keep list
-#drop_dups <- read.table("drop_dups.txt", header = TRUE)
-#Check all drop identities are in sample_key and remove them
-#sum(drop_dups$sid %in% sample_key$sid) == nrow(drop_dups)
-#[1] TRUE
-#sample_key_temp <- sample_key[sample_key$sid %notin% drop_dups$sid,]
-#nrow(sample_key_temp)
-#[1] 5162
-#sum(duplicated(sample_key_temp$vcf_id)) #Must be 0!
-#[1] 0
-    
-#participants$pid <- ifelse(participants$pid %in% dups_df$pid, NA, as.character(participants$pid))
-#sum(is.na(participants$pid))
-#[1] 33
-    
-#sum(duplicated(sample_key$pid))
-#[1] 38
-#Because sample key has duplicated pids, must mark duplicates (based on VCF) before mergeing with participants file based on pid
-#dups <- as.matrix(read.table("dups.txt", header = TRUE))
-#sample_key[sample_key$vcf_id %in% dups[,1:3], c("pid", "sid", "vcf_id")]
-#dups_df <- sample_key[sample_key$vcf_id %in% dups[,1:3] & sample_key$vcf_id %notin% dups[,5], c("vcf_id", "sid", "pid")]
-#
-#sample_key$duplicate <- ifelse(sample_key$vcf_id %in% dups_df$vcf_id, "duplicate", NA)
-#table(sample_key$duplicate)
-#[1] 37
-#
-#sum(participants$pid %in% dups_df$pid)
-#[1] 33
-
 
 
 
