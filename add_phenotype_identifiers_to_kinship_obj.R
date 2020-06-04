4a815de4844c4ba24842b93750fdb53321cc6491
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-04 13:56:18 -0700

    Update add_phenotype_identifiers_to_kinship_obj.R

--- add_phenotype_identifiers_to_kinship_obj.R
+++ add_phenotype_identifiers_to_kinship_obj.R
@@ -21,7 +21,7 @@ for(i in 1:nrow(kinship)){
   RE1 <- phenotype[phenotype$sid == kinship$ID1[i], "race_or_ethnicity"]
   RE2 <- phenotype[phenotype$sid == kinship$ID2[i], "race_or_ethnicity"]
   kinship$race_or_ethnicity[i] <- ifelse(RE1 == "white" & RE2 == "white", "white", ifelse(RE1 == "black" & RE2 == "black", "black", ifelse(RE1 == RE2, "match", "different_race_or_ethnicity")))
-  if(i %% 100000 == 0)
+  if(i %% 100000 == 0){
      print("1,000,000 rows run")
      }
   }
