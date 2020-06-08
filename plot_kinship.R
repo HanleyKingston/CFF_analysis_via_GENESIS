e4d9e6784f8aea4d7901fac56adc0c773f66b74a
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-08 15:01:22 -0700

    Update plot_kinship.R

--- plot_kinship.R
+++ plot_kinship.R
@@ -1,46 +1,52 @@
 #! /usr/bin/env Rscript
 library(argparser)
 library(magrittr)
+
+#Read arguments
 argp <- arg_parser("Generate kinship plot") %>%
-  add_argument("pcrelate_file", help = "PC-AiR file (.rds)") %>%
-  add_argument("--out_prefix", help = "Prefix for output files",
-               default = "") %>%
+  add_argument("pcrelate_file", help = "PC-Relate file (.rds)") %>%
+  add_argument("--out_prefix", help = "Prefix for output files", default = "") %>%
   add_argument("--group", help = "grouping variable") %>%
-  add_argument("--king", flag = TRUE, help = "Is input file format from King?") %>%
-  add_argument("--x_axis", default = "k0") %>%
-  add_arguemnt("--y_axis", default = "kin") %>%
+  add_argument("--is_king", flag = TRUE, help = "Is input file format from King?") %>%
+  add_argument("--x_axis", default = "k0", help = "x variable") %>%
+  add_argument("--y_axis", default = "kin", help = "y variable")
                
 argv <- parse_args(argp)
+print(argv)
 
 library(ggplot2)
 library(SNPRelate)
 sessionInfo()
 print(argv)
 
-out_prefix <- argv$out_prefix
 
 pcrel <- readRDS(argv$pcrelate_file)
+out_prefix <- argv$out_prefix
 
-kinship <- ifelse(argv$keep_king, snpgdsIBDSelection(KingRel), pcrel$kinBtwn)
 
-png(paste0(out_prefix, "_kinship.png"))
-ggplot(kinship, aes(k0, kin)) +
+if(argv$is_king == TRUE){
+	kinship <- snpgdsIBDSelection(KingRel)
+	} else {
+	kinship <- pcrel$kinBtwn
+	}
+
+if(argv$is_king & argv$x_axis == "k0" & argv$y_axis == "kin"){
+  argv$x_axis <- "IBS0"
+  argv$y_axis <- "kinship"
+  }
+
+#if (!is.na(argv$group)) {
+#  group <- argv$group
+#  } else {
+#  ## make up dummy group
+#  group <- "group"
+#  kinship$group <- "NA"
+#}
+
+png(paste0(argv$out_prefix, "_kinship.png"))
+ggplot(kinship, aes_string(argv$x_axis, argv$y_axis, group = group)) +
     geom_hline(yintercept=2^(-seq(3,9,2)/2), linetype="dashed", color = "grey") +
     geom_point(alpha=0.2) +
     ylab("kinship estimate") +
     ggtitle("kinship")
-dev.off()
-
-#For KING plot:
-KingRel <- readRDS(file = "king_obj.rds")
-
-
-head(kinship)
-
-png("king_plot.png")
-ggplot(kinship, aes(IBS0, kinship)) +
-    geom_hline(yintercept=2^(-seq(3,9,2)/2), linetype="dashed", color="grey") +
-    geom_point(alpha=0.2) +
-    ylab("kinship estimate") +
-
 dev.off()
