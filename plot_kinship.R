d1c0aeb5d35d99e54f73eb958ecb7a125afa134e
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-08 15:19:24 -0700

    Update plot_kinship.R

--- plot_kinship.R
+++ plot_kinship.R
@@ -6,7 +6,7 @@ library(magrittr)
 argp <- arg_parser("Generate kinship plot") %>%
   add_argument("pcrelate_file", help = "PC-Relate file (.rds)") %>%
   add_argument("--out_prefix", help = "Prefix for output files", default = "") %>%
-  add_argument("--group", help = "grouping variable") %>%
+  add_argument("--group", help = "grouping variable - a column in king or pc-relate dataframe") %>%
   add_argument("--is_king", flag = TRUE, help = "Is input file format from King?") %>%
   add_argument("--x_axis", default = "k0", help = "x variable") %>%
   add_argument("--y_axis", default = "kin", help = "y variable")
@@ -20,14 +20,14 @@ sessionInfo()
 print(argv)
 
 
-pcrel <- readRDS(argv$pcrelate_file)
+rel <- readRDS(argv$pcrelate_file)
 out_prefix <- argv$out_prefix
 
 
 if(argv$is_king == TRUE){
-	kinship <- snpgdsIBDSelection(KingRel)
+	kinship <- snpgdsIBDSelection(rel)
 	} else {
-	kinship <- pcrel$kinBtwn
+	kinship <- rel$kinBtwn
 	}
 
 if(argv$is_king & argv$x_axis == "k0" & argv$y_axis == "kin"){
@@ -35,16 +35,14 @@ if(argv$is_king & argv$x_axis == "k0" & argv$y_axis == "kin"){
   argv$y_axis <- "kinship"
   }
 
-#if (!is.na(argv$group)) {
-#  group <- argv$group
-#  } else {
-#  ## make up dummy group
-#  group <- "group"
-#  kinship$group <- "NA"
-#}
+if (!is.na(argv$group)) {
+  group <- argv$group
+  } else {
+  group <- NULL
+}
 
 png(paste0(argv$out_prefix, "_kinship.png"))
-ggplot(kinship, aes_string(argv$x_axis, argv$y_axis, group = group)) +
+ggplot(kinship, aes_string(argv$x_axis, argv$y_axis, color = group)) +
     geom_hline(yintercept=2^(-seq(3,9,2)/2), linetype="dashed", color = "grey") +
     geom_point(alpha=0.2) +
     ylab("kinship estimate") +
