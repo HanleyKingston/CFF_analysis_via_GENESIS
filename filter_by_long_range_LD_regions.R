4f54e1f3a619b88663d14291c893a95bac79fdf1
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-15 13:27:18 -0700

    Update filter_by_long_range_LD_regions.R

--- filter_by_long_range_LD_regions.R
+++ filter_by_long_range_LD_regions.R
@@ -1,10 +1,15 @@
-#' @param build Genome build to use when identifying regions to exclude from PCA because of high correlation (HLA, LCT, inversions)
-#' @rdname filterVariants
-#'
-#' @import SeqArray
-#' @importFrom utils data
-#' @export
-filterByPCAcorr <- function(gds, build="hg19", verbose=TRUE) {
+#CFF_sid_onlyGT.gds
+
+library(SeqArray)
+library(utils)
+
+argp <- arg_parser("Run KING-robust") %>%
+  add_argument("gds_file", help = "GDS file") %>%
+
+
+gds <- argv$gds_file
+
+filterByPCAcorr <- function(gds, build="hg38", verbose=TRUE) {
     filt <- get(data(list=paste("pcaSnpFilters", build, sep="."), package="GWASTools"))
     chrom <- seqGetData(gds, "chromosome")
     pos <- seqGetData(gds, "position")
@@ -12,5 +17,5 @@ filterByPCAcorr <- function(gds, build="hg19", verbose=TRUE) {
     for (f in 1:nrow(filt)) {
         pca.filt[chrom == filt$chrom[f] & filt$start.base[f] < pos & pos < filt$end.base[f]] <- FALSE
     }
-    seqSetFilter(gds, variant.sel=pca.filt, action="intersect", verbose=verbose)
+    seqSetFilter(gds, variant.sel=pca.filt, action="intersect", verbose=TRUE)
 }
