6d442273aee0e25b877ed308e5c59a2dc2598e7e
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-15 23:29:17 -0700

    Update ld_pruning.R

--- ld_pruning.R
+++ ld_pruning.R
@@ -39,4 +39,17 @@ snpset <- snpgdsLDpruning(gds,
 # convert list with one element per chrom to vector
 pruned <- unlist(snpset, use.names=FALSE)
 
+filterByPCAcorr <- function(gds, build="hg38", verbose=TRUE) {
+    filt <- get(data(list=paste("pcaSnpFilters", build, sep="."), package="GWASTools"))
+    chrom <- seqGetData(gds, "chromosome")
+    pos <- seqGetData(gds, "position")
+    pca.filt <- rep(TRUE, length(chrom))
+    for (f in 1:nrow(filt)) {
+        pca.filt[chrom == filt$chrom[f] & filt$start.base[f] < pos & pos < filt$end.base[f]] <- FALSE
+    }
+    seqSetFilter(gds, variant.sel=pca.filt, action="intersect", verbose=TRUE)
+}
+
+PCAcorr_snps <- seqGetData(gds, "variant.id")
+
 saveRDS(pruned, file=out.file)
