ffa8612a3a55f7bb9f8fab8b0e0400462e95d2b4
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-23 16:40:26 -0700

    Update and rename SaKnorm_assoc_test.sh to sex_assoc_test.sh

---
+++ sex_assoc_test.sh
@@ -0,0 +1,10 @@
+  
+#!/bin/bash
+
+for f in {1..22}; do
+echo "Assoc_testing '$f' "
+R -q --vanilla --args CFF_sid_onlyGT.gds annot.rds 6_18pcr_mat.rds sex_registry binomial --out_prefix "sex_$f" --covars "PC1 PC2 PC3 site age_cohort F508_count" --variant_id var_filter_SNVs_MAF0.05.rds --sample_id keep_samples_noTwins.rds --chromosome "$f" < assoc_test.R > assoc_test.log &
+sleep 30
+done
+
+#Run with: sh assoc_test.sh
