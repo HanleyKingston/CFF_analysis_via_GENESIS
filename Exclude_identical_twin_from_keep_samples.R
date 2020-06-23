e9e1b3918283f2418ebadf21afd8b6fd6d4f9a38
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-23 14:02:55 -0700

    Update Exclude_identical_twin_from_keep_samples.R

--- Exclude_identical_twin_from_keep_samples.R
+++ Exclude_identical_twin_from_keep_samples.R
@@ -1,16 +1,18 @@
+"%notin%" <- Negate("%in%")
+
+
 #Identify identical twins & remove from analysis
 
 #Read in PCrelate object and existing keep_samples vector
-pcrel2 <- readRDS(file = "CFF_LDsqrt0.1pcr_obj.rds")
+pcrel2 <- readRDS(file = "6,18pcr_obj.rds")
 kinship <- pcrel2$kinBtwn
 keep_samples <- readRDS(file = "keep_samples.rds")
 
 #Find likely identical twins
-range(kinship$kin)
-kinship[kinship$kin > 0.40, c("kin", "k2", "ID1", "ID2")]
+kinship[kinship$kin > 0.45, c("kin", "k2", "ID1", "ID2")]
 
 #Remove identical twins from keep_samples vector
-exclude_twins <- kinship[kinship$kin > 0.5, "ID1"] #ID1 is always the lowest of the 2 number IDs
+exclude_twins <- kinship[kinship$kin > 0.45, "ID1"] #ID1 is always the lowest of the 2 number IDs
 "%notin%" <- Negate("%in%")
 keep_samples_noTwins <- keep_samples[keep_samples %notin% exclude_twins]
 
