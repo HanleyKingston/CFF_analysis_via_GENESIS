b2825a34f402ca4ab9fb1f3adeb6828eb754197f
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-23 10:37:07 -0700

    Update assoc_test.sh

--- assoc_test.sh
+++ assoc_test.sh
@@ -2,7 +2,7 @@
 
 for f in {1..22}; do
 echo "Assoc_testing '$f' "
-R -q --vanilla --args CFF_sid_onlyGT.gds annot.rds 6_18pcr_mat.rds F508_count poisson --out_prefix "$f" --covars "PC1 PC2 PC3 sex_wgs birthdate_year site" --variant_id var_filter_SNVs_MAF0.05.rds --sample_id keep_samples.rds --chromosome "$f" < assoc_test2.R &
+R -q --vanilla --args CFF_sid_onlyGT.gds annot.rds 6_18pcr_mat.rds F508_count poisson --out_prefix "$f" --covars "PC1 PC2 PC3 sex_wgs birthdate_year site" --variant_id var_filter_SNVs_MAF0.05.rds --sample_id keep_samples.rds --chromosome "$f" < assoc_test2.R > assoc_test.log &
 sleep 30
 done
 
