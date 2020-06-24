bc366e713559e50c6219a653d0fc95a22dd0046e
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-23 19:28:34 -0700

    Update qq_and_manhattan_plots.R

--- qq_and_manhattan_plots.R
+++ qq_and_manhattan_plots.R
@@ -1,4 +1,4 @@
-#R -q --vanilla --args assoc.rds --out_prefix CFF_F508 --ymin 1 --ymax 10 < assoc_plots.R &
+#R -q --vanilla --args assoc.rds --out_prefix CFF_F508 --M_ymin 1 --M_ymax 10 < assoc_plots.R &
 
 library(qqman)
 library(utils)
@@ -8,8 +8,10 @@ argp <- arg_parser("Create qq-plot and Manhattan plot from association results")
   add_argument("assoc_file", help = "file of association test results") %>%
   add_argument("--out_prefix", help = "Prefix for output files",
                default = "") %>%
-  add_argument("--ymin", help = "minimum log(y value) to show on Manhattan plot", default = NULL) %>%
-  add_argument("--ymax", help = "maximum log(y value) to show on Manhattan plot", default = NULL)
+  add_argument("--Q_ymin", help = "minimum log(y value) to show on qq-plot", default = NULL) %>%
+  add_argument("--Q_ymax", help = "maximum log(y value) to show on qq-plot", default = NULL) %>%
+  add_argument("--M_ymin", help = "minimum log(y value) to show on Manhattan plot", default = NULL) %>%
+  add_argument("--M_ymax", help = "maximum log(y value) to show on Manhattan plot", default = NULL)
 
 
 argv <- parse_args(argp)
@@ -18,23 +20,23 @@ assoc <- readRDS(argv$assoc_file)
 
 
 #Generate lambda
-lambda <- round(median(-2*log(assoc$Score.pval))/1.39, 3)
+lambda <- Lambda((assoc$Score.Stat)^2, df=1)
 print(lambda)
 
 #Generate qq-plot
 png(paste0(argv$out_prefix, "_qq_plot.png"))
-	qq(Assoc$Score.pval) #Need to add a title, but qq doesn't acept "main ="
+  qq(assoc$Score.pval) #Need to add a title, but qq doesn't acept "main ="
   legend("topleft", legend = paste("lambda =", lambda))
-  dev.off()
+dev.off()
   
   
 #generate Manhattan plot
-Assoc <- type.convert(Assoc) #Chromosomes must be numeric for manhattan function
+assoc <- type.convert(assoc) #Chromosomes must be numeric for manhattan function
 
 png(paste0(argv$out_prefix, "_manhattan_plot.png"))
-	manhattan(Assoc, snp = "variant.id", chr = "chr", bp = "pos", p = "Score.pval", main = paste(title), ylim = ylim, annotatePval = 10^(-4))
+  manhattan(assoc, snp = "variant.id", chr = "chr", bp = "pos", p = "Score.pval", main = paste(title), ylim = ylim, annotatePval = 10^(-4))
   legend("topleft", legend = paste("lambda =", lambda))
-  dev.off()
+dev.off()
 
 
 #Return a list of SNPs out of range based on ymax
