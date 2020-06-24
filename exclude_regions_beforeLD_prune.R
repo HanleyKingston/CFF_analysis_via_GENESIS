5f5c97a385bbee0df3cb3a105917c05b214a1cac
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-24 10:07:16 -0700

    Update exclude_regions_beforeLD_prune.R

--- exclude_regions_beforeLD_prune.R
+++ exclude_regions_beforeLD_prune.R
@@ -1,5 +1,6 @@
 add_regions <- data.frame(t(c(7, 1, 159345973, "exclude_chr7_to_exclude_deltaF508")))
-colnames(add_regions) <- colnames(filt)
+colnames(add_regions) <- c("chrom", "start.base", "end.base", "comment")
+
 rownames(add_regions) <- "chr7"
 
 print(as.data.frame(add_regions))
