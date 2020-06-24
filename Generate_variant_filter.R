90a9003567b8d4414b8bada1a84dff04a0c153ed
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-24 09:12:22 -0700

    Update Generate_variant_filter.R

--- Generate_variant_filter.R
+++ Generate_variant_filter.R
@@ -1,5 +1,5 @@
 #This generates a list of variant IDs based on filter criteria... I generated a more and less stringent filter
-#both filters follows suggested filters from: https://gatk.broadinstitute.org/hc/en-us/articles/360035531112 (see:[C] Hard-filter SNPs on multiple expressions using VariantFiltration)
+#both filters follows VQSR & GATK hard filters:suggested filters from: https://gatk.broadinstitute.org/hc/en-us/articles/360035531112 (see:[C] Hard-filter SNPs on multiple expressions using VariantFiltration)
 #
 #moderate filter also excludes:
 # MAF < 0.01
@@ -76,14 +76,14 @@ table(flag.metric.df$flag.missByVar.0.0, useNA = "ifany")
 #114437649   5702195
 missByVar <- flag.metric.df$flag.missByVar.0.05 == FALSE
 
-#Exclude X-chromosome (this is probably happens based on other filters, but just to be safe)
+#Exclude X-chromosome
 table(flag.metric.df$chr == "X")
 #    FALSE      TRUE
 #115537943   4601901
 noX <- flag.metric.df$chr != "X"
 
 
-##Get minor allele frequency - note: PC-pruning also has an option to filter by MAF, but I am doing it here for consistency with variants I will use in association testing)
+##Optional: plot minor allele frequency - note: PC-pruning also has an option to filter by MAF, so I will not include this in the filter
 library(SeqVarTools)
 gds <- seqOpen("CFF_sid_onlyGT.gds")
 
@@ -93,8 +93,6 @@ sum(maf <= 0.05)
 #[1] 110327959
 sum(maf > 0.05)
 #[1] 9811885 #These I will keep
-
-
 
 #Plot MAF
 png("minor_allele_freq_hist.png")
@@ -108,40 +106,35 @@ nrow(flag.metric.df)
 #[1] 120139844
 
 #If file has multiple varaint ID's, make sure there are no repeats:
-sum(flag.metric.df$variant_id_gds) == nrow(flag.metric.df$variant_id_gds) #Check that all varaint IDs are unique
-var_filter_moderate_gds.temp <- flag.metric.df[flag.metric.df$variant.id, "variant_id_gds"]
+length(unique(flag.metric.df$variant_id)) == length(flag.metric.df$variant_id) #Check that all varaint IDs are unique
 
 
 ## Extract the correct varaint ID... IMPORTANT: must make sure gds varaint IDs and flag.metric IDs match by position and chromosome (see "Check_or_match_gdsIDs_to_Filters.R")
 
 ### Moderate
-var_filter_moderate_gds.temp <- flag.metric.df$variant_id_gds[RankSum & MQ & FS & SOR & qual & QD & (snvPASS | indelPASS) & noX] 
-var_filter_SNPs_and_indels_MAF0.01 <- intersect(seqGetData(gds, "variant.id")[maf > 0.01], var_filter_moderate_gds.temp)
-length(var_filter_SNPs_and_indels_MAF0.01)
-#[1] 10418664
+var_filter_SNVs_and_indels <- flag.metric.df$variant_id[RankSum & MQ & FS & SOR & qual & QD & (snvPASS | indelPASS)] 
+length(var_filter_SNVs_and_indels)
+#[1] 100635332
+
 
 ### Stringent
-var_filter_stringent_gds.temp <- flag.metric.df$variant_id_gds[RankSum & MQ & FS & SOR & qual & QD & snvPASS & missByVar & biAllelic & noX]
-var_filter_SNVs_MAF0.05 <- intersect(seqGetData(gds, "variant.id")[maf > 0.05], var_filter_stringent_gds.temp)
-length(var_filter_SNVs_MAF0.05)
-#[1] 5490945
-
-
-### Just VQSR & GATK hard filters:
-VQSR_GATK_only <- flag.metric.df$variant_id_gds[RankSum & MQ & FS & SOR & qual & QD & (snvPASS | indelPASS)] 
+var_filter_SNVs <- flag.metric.df$variant_id[RankSum & MQ & FS & SOR & qual & QD & snvPASS & missByVar & biAllelic]
+length(var_filter_SNVs)
+#[1] 87672807
 
 
 #Note: not filtering by HW (should have already been done and I don't have the family info to control for relatedness yet)
 
 
-saveRDS(var_filter_SNPs_and_indels_MAF0.01, file = "var_filter__SNVs_and_indels_lowMAF.rds")
-saveRDS(var_filter_SNVs_MAF0.05, file = "var_filter_SNVs_MAF0.05.rds")
-saveRDS(VQSR_GATK_only, file = "var_filter_VQSR_GATK_only.rds")
+saveRDS(var_filter_SNVs_and_indels , file = "var_filter_SNVs_and_indels .rds")
+saveRDS(var_filter_SNVs, file = "var_filter_SNVs.rds")
 
 
 #To test filter:
-seqSetFilter(gds, variant.id = var_filter_SNVs_MAF0.05)
+seqSetFilter(gds, variant.id = var_filter_SNVs)
 ## of selected variants: 5,490,945
+seqResetFilter(gds)
+## of selected variants: 87,672,807
 
 
 
