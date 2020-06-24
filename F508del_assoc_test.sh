88d076b6d1f9c617725f953fe466955b9c12f24a
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-23 19:16:13 -0700

    Update F508del_assoc_test.sh

--- F508del_assoc_test.sh
+++ F508del_assoc_test.sh
@@ -2,9 +2,9 @@
 
 for f in {1..22}; do
 echo "Assoc_testing '$f' "
-R -q --vanilla --args CFF_sid_onlyGT.gds annot.rds 6_18pcr_mat.rds F508_count poisson --out_prefix "F508del$f" --covars "PC1 PC2 PC3 site" --variant_id var_filter_SNVs_MAF0.05.rds --sample_id keep_samples_noTwins.rds --chromosome "$f" < assoc_test.R > F508del_assoc_test.log &
+R -q --vanilla --args CFF_sid_onlyGT.gds annot.rds 6_18pcr_mat.rds F508_count poisson --out_prefix "F508del_chr$f" --covars "PC1 PC2 PC3 site" --variant_id var_filter_SNVs_MAF0.05.rds --sample_id keep_samples_noTwins.rds --chromosome "$f" < assoc_test.R > F508del_assoc_test.log &
 sleep 30
 done
 
-#Run with: sh assoc_test.sh
+#Run with: sh F508del_assoc_test.sh
 
