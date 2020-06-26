f87c0e2d53e6f3c504379145d006cc4a5cfa4eb9
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-25 23:35:01 -0700

    Update calculate_snp_pc_corr.sh

--- SNP_correlations/calculate_snp_pc_corr.sh
+++ SNP_correlations/calculate_snp_pc_corr.sh
@@ -6,7 +6,7 @@
 
 module load conda
 
-R -q --no-save --args --pca-file 6_25_1itpcair.rds --gds-file CFF_sid_onlyGT.gds --block-size 32768 --chromosome $SGE_TASK_ID --outfile /home/hkings/DATA/pc_correlations6_25/snp_corr_chr${SGE_TASK_ID}.rds < calculate_snp_pc_corr.R
+R -q --no-save --args --pca-file 6_25_1itpcair.rds --gds-file CFF_sid_onlyGT.gds --block-size 32768 --chromosome $SGE_TASK_ID --outfile /home/hkings/DATA/PC_correlations6_25/snp_corr_chr${SGE_TASK_ID}.rds < calculate_snp_pc_corr.R
 
 
 
