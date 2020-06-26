17ae64860afd143f3a781fb67111fb2e7412af76
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-25 17:56:33 -0700

    Update plot_snp_pc_corr.sh

--- SNP_correlations/plot_snp_pc_corr.sh
+++ SNP_correlations/plot_snp_pc_corr.sh
@@ -7,4 +7,4 @@ module load conda
 
 R -q --no-save --args --infile-prefix /home/hkings/DATA/PC_correlations6,18/snp_corr --outfile /home/hkings/DATA/PC_correlations6,18/snp_corr.png < plot_snp_pc_corr.R
 
-#Run with: qsub -q new.q -v R_LIBS=/home/amstilp/devel/analysis_pipeline_cff_wgs/R_library calculate_snp_pc_corr.sh plot_snp_pc_corr.sh
+#Run with: qsub -q new.q -v R_LIBS=/home/amstilp/devel/analysis_pipeline_cff_wgs/R_library plot_snp_pc_corr.sh
