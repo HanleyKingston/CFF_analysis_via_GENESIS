98aa05662ca622bc695038cae42fe7f3278c0741
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-26 18:09:49 -0700

    Update ld_pruning.R

--- ld_pruning.R
+++ ld_pruning.R
@@ -6,7 +6,7 @@ sessionInfo()
 # read arguments
 argp <- arg_parser("LD pruning")
 argp <- add_argument(argp, "gds_file", help="GDS file")
-argp <- add_argument(argp, "--out_file", help="output file name", default="pruned_snps.rds")
+argp <- add_argument(argp, "--out_prefix", help="output file name", default="")
 argp <- add_argument(argp, "--sample_id", help="RDS file with vector of sample.id to include")
 argp <- add_argument(argp, "--variant_id", help="RDS file with vector of variant.id to include")
 argp <- add_argument(argp, "--maf", help="minimum MAF for variants to include", default=0.05)
@@ -14,8 +14,8 @@ argp <- add_argument(argp, "--missing", help="maximum missing call rate for vari
 argp <- add_argument(argp, "--r_threshold", help="r threshold for LD", default=sqrt(0.1))
 argp <- add_argument(argp, "--window_size", help="window size in Mb", default=10)
 argp <- add_argument(argp, "--autosome_only", help = "whether to include autosomes only or also X-chromosome", default=TRUE)
-argp <- add_argument(argp, "--build", help = "map build to filter problematic regions")
 argp <- add_argument(argp, "--exclude_regions", help = "a dataframe of regions to exclude from pruned_SNPs, with colnames: chrom, start.base, end.base, comment ... note: only used if build is also provided")
+argp < -add_argument(argp, "--chromosome", help = "chromosome number")
 argv <- parse_args(argp)
 print(argv)
 
@@ -24,28 +24,18 @@ gds.file <- argv$gds_file
 out.file <- argv$out_file
 sample.id <- if (!is.na(argv$sample_id)) readRDS(argv$sample_id) else NULL
 variant.id <- if (!is.na(argv$variant_id)) readRDS(argv$variant_id) else NULL
-exclude_regions <- if( !is.na(argv$exclude_regions)) readRDS(argv$exclude_regions) else NULL
 
 
 # open GDS file
 gds <- seqOpen(gds.file)
   
-if( !is.na(argv$build)){ #Need to check what this will do if not build is supplied
-    filt <- get(data(list=paste("pcaSnpFilters", argv$build, sep="."), package="GWASTools"))
-    filt <- rbind(filt, exclude_regions)
-    chrom <- seqGetData(gds, "chromosome")
-    pos <- seqGetData(gds, "position")
-    pca.filt <- rep(TRUE, length(chrom))
-    for (f in 1:nrow(filt)) {
-        pca.filt[chrom == filt$chrom[f] & filt$start.base[f] < pos & pos < filt$end.base[f]] <- FALSE
-    }
-    seqSetFilter(gds, variant.sel=pca.filt, action="intersect", verbose=TRUE)
+if (!ia.na(argv$chromosome) {
+  seqSetFilterChrom(gds, chromosome)
+  chrom_vars <- seqGetData(gds, "variant.id")
+  variant.id <- intersect(variant.id, chrom_vars)
+  seqResetFilter(gds)
 }
-  
-PCAcorr_snps <- seqGetData(gds, "variant.id")
-saveRDS(PCAcorr_snps, "PCAcorr_snps.rds")
-
-
+    
 # run LD pruning
 snpset <- snpgdsLDpruning(gds,
                           sample.id = sample.id,
@@ -61,10 +51,6 @@ snpset <- snpgdsLDpruning(gds,
 # convert list with one element per chrom to vector
 pruned <- unlist(snpset, use.names=FALSE)
 
-
-pruned <- intersect(pruned, PCAcorr_snps)
-
 print(length(pruned))
 
-
-saveRDS(pruned, file=out.file)
+saveRDS(pruned, paste0(argv$out_prefix, "pruned.rds"))
