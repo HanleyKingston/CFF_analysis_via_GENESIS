be9994552865a294644b16b4e6ad851df2b373d1
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-30 14:23:31 -0700

    Create F508del_assoc_test_noPCs.sh

---
+++ assoc/F508del_assoc_test_noPCs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for f in {1..22}; do
+echo "Assoc_testing '$f' "
+R -q --vanilla --args CFF_sid_onlyGT.gds annot.rds --out_prefix "F508del_noPCs_chr_$f" --variant_id SNPS_bi_GATK_VQSR_MAF0.05_miss0.05.rds --sample_id keep_samples_noTwins.rds --chromosome "$f" --null_model F508del_noPCs_nullmod.rds < assoc_test.R > F508del_assoc_test_noPCs.log &
+
+sleep 30
+done
+
+#Run with: sh sex_assoc_test.sh
