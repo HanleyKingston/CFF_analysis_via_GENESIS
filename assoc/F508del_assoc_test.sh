b8bcb772419babf429345bf1c9bb4fdfbd94ab3c
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-30 10:25:05 -0700

    Update F508del_assoc_test.sh

--- assoc/F508del_assoc_test.sh
+++ assoc/F508del_assoc_test.sh
@@ -4,10 +4,8 @@
 #$ -j y
 #$ -t 1-22
 
-for f in {1..22}; do
-echo "Assoc_testing '$f' "
-R -q --vanilla --args CFF_sid_onlyGT.gds annot.rds 6_26pcr_mat.rds F508_count poisson --out_prefix F508del_chr_${SGE_TASK_ID} --covars "PC1 PC2 PC3 PC4 site" --variant_id SNPS_bi_GATK_VQSR_MAF0.05_miss0.05.rds --sample_id keep_samples_noTwins.rds --chromosome $SGE_TASK_ID < assoc_test.R > F508del_assoc_test${SGE_TASK_ID}.log &
-sleep 30
-done
+module load conda
+
+R -q --no-save --args CFF_sid_onlyGT.gds annot.rds 6_26pcr_mat.rds F508_count poisson --out_prefix F508del_chr_${SGE_TASK_ID} --covars "PC1 PC2 PC3 PC4 site" --variant_id SNPS_bi_GATK_VQSR_MAF0.05_miss0.05.rds --sample_id keep_samples_noTwins.rds --chromosome $SGE_TASK_ID < assoc_test.R > F508del_assoc_test${SGE_TASK_ID}.log &
 
 #qsub -q new.q F508del_assoc_test.sh
