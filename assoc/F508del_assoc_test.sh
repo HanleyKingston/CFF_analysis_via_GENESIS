ef137a8e5bc981121b229d6cd617234d4f47a7fc
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-06-30 14:24:31 -0700

    Update F508del_assoc_test.sh

--- assoc/F508del_assoc_test.sh
+++ assoc/F508del_assoc_test.sh
@@ -1,11 +1,10 @@
-#$ -S /bin/sh
-#$ -N F508del_assoc_test
-#$ -cwd
-#$ -j y
-#$ -t 1-22
+#!/bin/bash
 
-module load conda
+for f in {1..22}; do
+echo "Assoc_testing '$f' "
+R -q --vanilla --args CFF_sid_onlyGT.gds annot.rds --out_prefix "F508del_chr_$f" --variant_id SNPS_bi_GATK_VQSR_MAF0.05_miss0.05.rds --sample_id keep_samples_noTwins.rds --chromosome "$f" --null_model F508del_nullmod.rds < assoc_test.R > F508del_assoc_test.log &
 
-R -q --no-save --args CFF_sid_onlyGT.gds annot.rds 6_26pcr_mat.rds F508_count poisson --out_prefix F508del_chr_${SGE_TASK_ID} --covars "PC1 PC2 PC3 PC4 site" --variant_id SNPS_bi_GATK_VQSR_MAF0.05_miss0.05.rds --sample_id keep_samples_noTwins.rds --chromosome $SGE_TASK_ID < assoc_test.R > F508del_assoc_test${SGE_TASK_ID}.log &
+sleep 30
+done
 
-#qsub -q new.q F508del_assoc_test.sh
+#Run with: sh sex_assoc_test.sh
