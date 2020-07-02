1c51c68e1891a5932911d46fe332b2ba9ae42a66
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-07-02 10:40:09 -0700

    Update change_gds_ids.R

--- change_gds_ids.R
+++ change_gds_ids.R
@@ -2,6 +2,8 @@ library(SeqArray)
 
 gds.id <- seqGetData(seqOpen("CFF_5134_onlyGT.gds"), "sample.id")
 sample_key <- read.table("sample_names_key.txt", header = TRUE)
+nrow(sample_key)
+#[1] 5199
 
 sum(duplicated(sample_key$vcf_id)) #Must be 0, otherwise, can't compair to gds file
 #[1] 0
@@ -12,10 +14,20 @@ nrow(sample_key)
 
 identical(as.character(sample_key$vcf_id), as.character(gds.id)) #This must be TRUE!
 
-sid_list <- as.vector(as.character(sample_key[sample_key$vcf_id %in% gds.id, "sid"]))
+sid_list <- as.character(sample_key$sid)
 
 class(sid_list)
 #[1] "character"
+
+#Spot check:
+sid_list[1200]
+#[1] "S38756"
+gds.id[1200]
+#[1] "JHU_TSS_592_2003_1"
+sample_key[1200,]
+#        pid    sid             vcf_id
+#4203 P82544 S38756 JHU_TSS_592_2003_1
+
 
 #Close gds file and reopen in write mode. Note: make a copy of the gds file and rename it "CFF_sid_onlyGT.gds"
 gdsfmt::showfile.gds(closeall=TRUE)
