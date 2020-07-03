e6ef3e5c0de2b4a009810daf25f1e62c0084ac0d
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-07-02 17:49:53 -0700

    Update combine_seperate_chr_files.R

--- combine_seperate_chr_files.R
+++ combine_seperate_chr_files.R
@@ -1,13 +1,12 @@
 args <- commandArgs(trailingOnly = TRUE)
 
-chr <- readRDS(paste0(args[1], "chr1_pruned.rds"))
+chr <- readRDS(paste0(args[1], "chr_1pruned.rds"))
 
 for(i in 2:22){
 chr_temp <- readRDS(paste0(args[1], "chr_", i, "pruned.rds"))
 chr <- append(chr, chr_temp)
 }
 
- str(chr)
- #int [1:171975] 1683 2190 2215 3755 6388 10751 10852 11502 13043 13086 ...
+str(chr)
 
 saveRDS(chr, paste0(args[1], "prunedSNPs.rds"))
